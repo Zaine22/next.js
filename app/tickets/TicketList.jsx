b8831bcb14eed5b06e1beff8979d3eea9ce6bd70
Zaine <57561694+Zaine22@users.noreply.github.com>
2024-02-04 07:30:23 +0630

    lesson 12

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import { resolve } from "styled-jsx/css";
+
 async function getTickets() {
-  const res = await fetch("http://localhost:4000/tickets");
+  //imitate delay
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 60, //use 0 to opt out of using cache
+    },
+  });
   return res.json();
 }
 
@@ -9,11 +18,13 @@ export default async function TicketList() {
     <>
       {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority}priority
-          </div>
+          <Link href={`tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority}priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
@@ -22,3 +33,5 @@ export default async function TicketList() {
     </>
   );
 }
+
+//tickets/id
